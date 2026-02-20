a804e5d6a375cc73d45c93051fa3ca130a6a726c
omprakash8788 <omprakashkumar552@gmail.com>
2026-02-20 17:07:40 +0530

    update cors policy

--- backend/index.js
+++ backend/index.js
@@ -5,20 +5,23 @@ import "dotenv/config";
 import connectToMongoDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 // import testRoutes from "./routes/testRoutes.js";
-import testRoutes from './routes/testRoutes.js';
-import questionRoutes from './routes/questionRoutes.js'
-import userRoutes from './routes/userRoutes.js'
+import testRoutes from "./routes/testRoutes.js";
+import questionRoutes from "./routes/questionRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 // import questionRoutes from "./routes/questionRoutes.js";
 import adminRoutes from "./routes/adminRoutes.js";
 
 const app = express();
-const port = process.env.PORT
+const port = process.env.PORT || 4000;
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://online-exam-frontend-4cqa.onrender.com",
+    ],
     credentials: true,
-  })
+  }),
 );
 app.use(cookieParser());
 connectToMongoDB();
